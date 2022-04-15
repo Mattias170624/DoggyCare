30e536fe3d172a654e01f828258613f286f152ff
Mattias170624 <ExozTic_170624@outlook.com>
2022-04-15 19:16:11 +0200

    Added contact page with contact form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Comps/Header';
 import DogList from './Comps/DogList';
 import HomePage from './Comps/HomePage';
+import Contact from './Comps/Contact';
 import Footer from './Comps/Footer';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 
@@ -28,7 +29,7 @@ function App() {
             <Route exact path="/contact" element={
               <div>
                 <Header />
-
+                <Contact />
                 <Footer />
               </div>
             } />
