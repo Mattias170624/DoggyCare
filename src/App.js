826b64a9c25e6acda34d6f15cab334dcd8b7930e
Mattias170624 <ExozTic_170624@outlook.com>
2022-04-17 19:51:19 +0200

    Restructured App.js, added DogList with api call inside

--- src/App.js
+++ src/App.js
@@ -4,38 +4,32 @@ import DogList from './Comps/DogList';
 import HomePage from './Comps/HomePage';
 import Contact from './Comps/Contact';
 import Footer from './Comps/Footer';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <Header />
+      </header>
+
       <main>
-        <Router>
-          <Routes>
-            <Route exact path="/" element={
-              <div>
-                <Header />
-                <HomePage />
-                <Footer />
-              </div>
-            } />
-            <Route path="/dogList" element={
-              <div>
-                <Header />
-                <DogList />
-                <Footer />
-              </div>
-            } />
-            <Route exact path="/contact" element={
-              <div>
-                <Header />
-                <Contact />
-                <Footer />
-              </div>
-            } />
-          </Routes>
-        </Router>
+        <Routes>
+          <Route exact path="/" element={
+            <HomePage />
+          } />
+          <Route path="/dogList" element={
+            <DogList />
+          } />
+          <Route path="/contact" element={
+            <Contact />
+          } />
+        </Routes>
       </main>
+
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
--- src/Comps/DogList.jsx
+++ src/Comps/DogList.jsx
@@ -1,10 +1,62 @@
+import { useEffect, useState } from "react";
 import "../CompStyles/dogList.css"
 
-
 const DogList = () => {
+    let [dogData, setDogData] = useState([])
+
+    useEffect(() => {
+        fetchUsers();
+    }, [])
+
+    const fetchUsers = async () => {
+        const res = await fetch("https://api.jsonbin.io/b/625a9f27bc312b30ebe81991");
+        const data = await res.json();
+        try {
+            setDogData(data)
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
     return (
         <div id="dogList">
-            <h3>Doglist component</h3>
+            <section id="dogListImage">
+                <div id="dogListText">
+                    <h1>Our List</h1>
+                    <p>We currently have 78 dogs<br />
+                        in our listing. You can find more details<br />
+                        on each dog by clicking on their image.
+                    </p>
+                </div>
+            </section>
+
+            <section id="dogListContainer">
+                <h2>Our Current Dogs</h2>
+
+                <div id="dogListData">
+                    <ul id="dogUl">
+                        {dogData &&
+                            dogData.map((dog, index) => (
+                                <li key={index}>
+                                    <section className="dogImageBox">
+                                        <img src={dog.img} />
+                                        <h1>{dog.name}</h1>
+                                        <p>
+                                            Breed: {dog.breed}<br />
+                                            Sex: {dog.sex}<br />
+                                            Age: {dog.age}<br />
+                                        </p>
+                                        <br />
+                                        <p>
+                                            Owner: {dog.owner.name} {dog.owner.lastName} <br />
+                                            Number: {dog.owner.phoneNumber}<br />
+                                        </p>
+                                    </section>
+                                </li>
+                            ))}
+                    </ul>
+                </div>
+            </section>
         </div>
     );
 };
